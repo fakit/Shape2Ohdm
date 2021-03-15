ecb7189dd1f3b2ccfd6274573e13adfb67c44d8e
fakit <fakit@web.de>
2021-03-16 00:45:48 +0100

    new config files

---
+++ src/util/Shape2OhdmTool.java
@@ -0,0 +1,35 @@
+package util;
+import static util.Config.*;
+
+public class Shape2OhdmTool {
+
+
+    public static void main(String[] args)  {
+
+        if (args.length > 0) {
+            CONFIG_PATH = args[0];
+        }
+        if (args.length > 1) {
+            TABLENAME = args[1];
+        }
+        if (args.length > 2) {
+            System.err.println("at most two parameter required: CONFIG PATH and TABLENAME");
+            System.exit(1);
+        }
+        try {
+            ohdmexecute();
+        } catch(
+                Exception e)
+
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public static void ohdmexecute() throws Exception {
+        System.out.println("Start Importing Shape into Ohdm Database");
+        OhdmLoader ohdmLoader = new OhdmLoader();
+        ohdmLoader.importFromIntermediateIntoOhdm();
+        System.out.println("Importing Shape into Ohdm Database : DONE !");
+    }
+}
